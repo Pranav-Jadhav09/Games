dbfda87eb01ccb3263c9ad640f09cfb6f23697f0
Pranav Rajesh Jadhav <iamoutput09@gmail.com>
2024-02-13 13:59:09 +0530

    game logic added

--- Pig-Dice/script.js
+++ Pig-Dice/script.js
@@ -24,3 +24,94 @@ const holdBtn = document.querySelector(".btn-hold");
 //////////////////////////////
 // Required Variables
 let scores, currenctScore, activePlayer, playingState;
+
+//////////////////////////////
+// Initial Condition
+const initial = () => {
+  scores = [0, 0];
+  activePlayer = 0;
+  currenctScore = 0;
+  playingState = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add("hidden");
+
+  Player0El.classList.remove("player-winner");
+  Player1El.classList.remove("player-winner");
+
+  Player0El.classList.add("player-active");
+  Player1El.classList.remove("player-active");
+};
+initial();
+
+////////////////////////////
+// Player Switch Function
+const switchPlayer = () => {
+  document.getElementById(`current-${activePlayer}`).textContent = 0;
+
+  currenctScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+
+  Player0El.classList.toggle("player-active");
+  Player1El.classList.toggle("player-active");
+};
+
+////////////////////////////
+// Roll Dice Function
+rollBtn.addEventListener("click", () => {
+  if (playingState) {
+    // Generate random number
+    const dice = Math.trunc(Math.random() * 6) + 1;
+
+    // Display dice
+    diceEl.classList.remove("hidden");
+    diceEl.src = `./assets/dice-${dice}.png`;
+
+    // Check for rolled 1
+    if (dice !== 1) {
+      // Add to current Score
+      currenctScore += dice;
+      document.getElementById(`current-${activePlayer}`).textContent =
+        currenctScore;
+    } else {
+      // Switch Player
+      switchPlayer();
+    }
+  }
+});
+
+////////////////////////////
+// Hold btn Function
+holdBtn.addEventListener("click", () => {
+  if (playingState) {
+    // Add currentScore to active Player's score
+    scores[activePlayer] += currenctScore;
+
+    document.getElementById(`score-${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    // Check if player score is >= 100
+    if (scores[activePlayer] >= 100) {
+      // Game Over
+      playingState = false;
+      diceEl.classList.add("hidden");
+
+      document
+        .querySelector(`.player-${activePlayer}`)
+        .classList.add("player-winner");
+      document
+        .querySelector(`.player-${activePlayer}`)
+        .classList.add("player-active");
+    } else {
+      switchPlayer();
+    }
+  }
+});
+
+////////////////////////////
+// Reset btn Function
+newBtn.addEventListener("click", initial);
