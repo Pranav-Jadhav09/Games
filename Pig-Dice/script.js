d3e0321a173e2b22c54f9c4e1986f96a4166d642
Pranav Rajesh Jadhav <iamoutput09@gmail.com>
2024-02-13 13:35:24 +0530

    init()

--- Pig-Dice/script.js
+++ Pig-Dice/script.js
@@ -1,1 +1,26 @@
 ("use strict");
+
+/////////////////////////////////////
+// Selecting Elements of Each Player
+const Player0El = document.querySelector(".player-0");
+const Player1El = document.querySelector(".player-1");
+
+const current0El = document.getElementById("current-0");
+const current1El = document.getElementById("current-1");
+
+const score0El = document.querySelector("#score-0");
+const score1El = document.querySelector("#score-1");
+
+//////////////////////////////
+// Selecting Image
+const diceEl = document.querySelector(".dice");
+
+//////////////////////////////
+// Selecting Button
+const newBtn = document.querySelector(".btn-new");
+const rollBtn = document.querySelector(".btn-roll");
+const holdBtn = document.querySelector(".btn-hold");
+
+//////////////////////////////
+// Required Variables
+let scores, currenctScore, activePlayer, playingState;
